82e2ef68667f59b763379ef8665a982836d49328
lmavaiya <42064785+lmavaiya@users.noreply.github.com>
2019-11-11 14:32:36 +0530

    date to string

--- src/main/java/ngo/ngoapp/model/User.java
+++ src/main/java/ngo/ngoapp/model/User.java
@@ -16,53 +16,5 @@ public class User {
     private String password;
     private String date_of_birth;
 
-    public User(String name, String phone, String email, String password, String date_of_birth) throws Exception{
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.password = password;
-        this.date_of_birth = date_of_birth;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 
 }
