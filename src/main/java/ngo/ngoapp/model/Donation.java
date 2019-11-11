fc313a58434476782b01f27e55f613df59afc17d
lmavaiya <42064785+lmavaiya@users.noreply.github.com>
2019-11-11 17:58:52 +0530

    NGOController Update

--- src/main/java/ngo/ngoapp/model/Donation.java
+++ src/main/java/ngo/ngoapp/model/Donation.java
@@ -4,25 +4,57 @@ import java.util.Date;
 
 public class Donation {
     private String user_id;
+    private String ngo_id;
     private float amount;
     private Date donate_on;
     private String payment_mode;
 
-    public Donation(String user_id, float amount, Date donate_on) {
+    public Donation(String user_id, String ngo_id, float amount, Date donate_on, String payment_mode) {
         this.user_id = user_id;
+        this.ngo_id = ngo_id;
         this.amount = amount;
         this.donate_on = donate_on;
+        this.payment_mode = payment_mode;
     }
+
 
     public String getUser_id() {
         return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getNgo_id() {
+        return ngo_id;
+    }
+
+    public void setNgo_id(String ngo_id) {
+        this.ngo_id = ngo_id;
     }
 
     public float getAmount() {
         return amount;
     }
 
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
     public Date getDonate_on() {
         return donate_on;
+    }
+
+    public void setDonate_on(Date donate_on) {
+        this.donate_on = donate_on;
+    }
+
+    public String getPayment_mode() {
+        return payment_mode;
+    }
+
+    public void setPayment_mode(String payment_mode) {
+        this.payment_mode = payment_mode;
     }
 }
--- src/main/java/ngo/ngoapp/model/User.java
+++ src/main/java/ngo/ngoapp/model/User.java
@@ -7,14 +7,6 @@ import java.util.Date;
 
 @Document(collection = "user")
 public class User {
-    public User(String name, String phone, String email, String password, Date date_of_birth) {
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.password = password;
-        this.date_of_birth = date_of_birth;
-    }
-
     @Id
     private String id;
     private String name;
@@ -22,7 +14,16 @@ public class User {
     private String email;
     private String password;
     private Date date_of_birth;
+    private Date registered_on;
 
+    public User(String name, String phone, String email, String password, Date date_of_birth) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.date_of_birth = date_of_birth;
+        this.registered_on = new Date();
+    }
 
     public String getId() {
         return id;
@@ -72,6 +73,13 @@ public class User {
         this.date_of_birth = date_of_birth;
     }
 
+    public Date getRegistered_on() {
+        return registered_on;
+    }
+
+    public void setRegistered_on(Date registered_on) {
+        this.registered_on = registered_on;
+    }
 
 
 }
